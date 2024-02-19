900a28a4c6042daf38dfc511a67c61e58ec2c007
RajaaLebnaiti <rajaa.lebnaiti@usms.ac.ma>
2024-02-19 20:18:00 +0100

    Create text_to_morse_code.py

---
+++ Morse_Code/text_to_morse_code.py
@@ -0,0 +1,43 @@
+#all_the symbols
+symbols = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": ".-",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+}
+
+#the user has to tyoe a word
+print("Please enter your text \n")
+ask = input("type: ")
+
+
+length = len(ask)
+output = ""
+
+for i in range(length):
+    if ask[i] in symbols.keys():
+        output = output + " " + symbols.get(ask[i])
+
+print("\n le texte saisit est :",ask,"\n En Morse code : ",output)   
